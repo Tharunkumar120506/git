ae390907e724ef149ddd6905bf8293db0dd0c228
Tharunkumar120506 <tharunkumars.23ece@kongu.edu>
2025-06-19 16:05:29 +0530

    Day3

---
+++ reactproject/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Welcome from './Welcome'
+//import './App.css'
+
+function App() {
+  return(
+  <>
+  <Welcome name='tharun'/>
+  <Welcome country='India'/>
+  <h1>How are you</h1>
+  </>
+  )
+}
+
+export default App
