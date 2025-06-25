18ae7cf4fdd898db6a7ef721e0472300738ae9b3
Tharunkumar120506 <tharunkumars.23ece@kongu.edu>
2025-06-25 09:36:30 +0530

    Day8

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,9 +2,11 @@
 
  export const connectDB=async()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/students')
+        await mongoose.connect('mongodb+srv://THARUNKUMAR:tharun2006@cluster0.wdfiwfb.mongodb.net/sample_mflix')
         console.log('MongoDB connected')
-    } catch (error)
+    }
+    
+    catch (error)
     {
         console.log(error);
     }
--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import {connectDB} from './config/db.js'
 import mongoose from 'mongoose'
 import User from './model/user.js'
@@ -8,15 +9,12 @@ const app=express()
 connectDB()
 app.use(express.json())
 app.use(express.urlencoded())
-
+app.use(cors())
 const methodFind=(req,res,next)=>{
-    console.log(`${req.method}`);
+    console.log(`${req.method} ${req.url}`);
     next()
 }
 app.use(methodFind)
-app.get('/get/:id',async(req,res)=>{
-    const 
-})
 app.get('/get',async(req,res)=>{
     const user= await User.find()
     res.json(user)  
@@ -27,6 +25,7 @@ app.post('/post',async(req,res)=>{
     try{
         const newUser=new User(req.body)
         await newUser.save()
+        res.status(201).json(newUser)
         res.json(newUser)
     }catch(error){
         res.status(401).json(error)
